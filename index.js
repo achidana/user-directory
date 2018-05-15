9a41a9f088ccdca8c11aaeff19a723877b6dee44
ashwin <achidana@purdue.edu>
2018-05-15 11:22:49 -0400

    Commit+0

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const x=document.querySelector('button')
 x.addEventListener('click',function(){
+    debugger
     if(document.querySelector('#name').value=="")
     {
     alert("PLEASE ENTER NAME")
     return
     }
+
 const y= document.querySelector('#pick_me')
 y.textContent=document.querySelector('#name').value
 })
---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export c=0
+
+git add -A
+msg="Commit"+$c
+git commit -m $msg
+c++
+
+
+
+
